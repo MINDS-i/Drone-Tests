38c348451275782f0df87bb108eb3fc86801a46d
Brett Menzies <scimor5@gmail.com>
2016-03-04 11:11:23 -0800

    Import more of Arduino's macros

--- avrgccExperiments/avrLibs/Arduino.h
+++ avrgccExperiments/avrLibs/Arduino.h
@@ -3,6 +3,8 @@
 #include <stdint.h>
 #include <math.h>
 #define constrain(amt,low,high) ((amt)<(low)?(low):((amt)>(high)?(high):(amt)))
+#define min(a,b) ((a)<(b)?(a):(b))
+#define max(a,b) ((a)>(b)?(a):(b))
 typedef bool boolean;
 uint32_t micros();
 uint32_t millis();
