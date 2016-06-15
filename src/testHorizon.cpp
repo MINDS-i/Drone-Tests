7380248acdd51349f7a2f3b3dbc734181d32b619
Brett Menzies <scimor5@gmail.com>
2016-06-15 15:40:42 -0700

    Remove tests of removed horizon standby methods

--- src/testHorizon.cpp
+++ src/testHorizon.cpp
@@ -21,30 +21,8 @@ RCFilter engine(1.0, 1.0);
 
 const float DELTA_T = 5.0; // elapsed time is milliseconds to report
 
-bool noOutputInStandby(){
-	float torques[4] = {1.0, 1.0, 1.0, 1.0};
-
-	horizon.standby();
-	horizon.update(engine, DELTA_T, torques);
-	for(int i=0; i<4; i++){
-		FPASSERT(torques[i], 0.0);
-	}
-	return true;
-}
-
-bool regularOutput(){
-	float torques[4];
-	horizon.activate();
-	horizon.set(0.5, 0, 0, 0.5);
-	horizon.update(engine, DELTA_T, torques);
-	return true;
-}
-
 int main(){
-	TEST(noOutputInStandby);
-
 	float torques[4];
-	horizon.activate();
 	horizon.set(0.5, 0, 0, 0.5);
 	beginTest("Horizon calculation");
 	horizon.update(engine, DELTA_T, torques);
