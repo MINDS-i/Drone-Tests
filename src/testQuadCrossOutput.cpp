2106fadf89b965c12098222b1e627c7d26c6a6fd
Brett Menzies <scimor5@gmail.com>
2016-06-20 16:34:24 -0700

    Update test for new output order

--- src/testQuadCrossOutput.cpp
+++ src/testQuadCrossOutput.cpp
@@ -8,19 +8,36 @@
 #include "output/QuadCrossOutput.h"
 
 /*
-cross quad copter input model:
-[1 1 -1 -1
- 1 -1 -1 1
- 1 -1 1 -1
- 1 1 1 1]
+ inputs:
+ 0: pitch ccw+
+ 1: roll ccw+
+ 2: z ccw+
+ 3: summed output force
+
+ outputs:
+  ^ Forward ^
+  ---     ---
+ | 2 |   | 0 |
+  ---\ ^ / ---
+      XXX
+      XXX
+  ---/   \---
+ | 1 |   | 3 |
+  ---     ---
+  0 - counter clockwise
+  1 - counter clockwise
+  2 - clockwise
+  3 - clockwise
 */
 const float testData[][2][4] {
     {{0.0,0.0,0.0,0.0},{ 0.00, 0.00, 0.00, 0.00}},
-    {{1.0,0.0,0.0,0.0},{ 0.25, 0.25,-0.25,-0.25}},
-    {{0.0,1.0,0.0,0.0},{ 0.25,-0.25,-0.25, 0.25}},
-    {{0.0,0.0,1.0,0.0},{ 0.25,-0.25, 0.25,-0.25}},
+    {{1.0,0.0,0.0,0.0},{ 0.25,-0.25, 0.25,-0.25}},
+    {{1.0,0.0,0.0,1.0},{ 0.50, 0.00, 0.50, 0.00}},
+    {{0.0,1.0,0.0,0.0},{-0.25, 0.25, 0.25,-0.25}},
+    {{0.0,1.0,0.0,1.0},{ 0.00, 0.50, 0.50, 0.00}},
+    {{0.0,0.0,1.0,0.0},{ 0.25, 0.25,-0.25,-0.25}},
+    {{0.0,0.0,1.0,1.0},{ 0.50, 0.50, 0.00, 0.00}},
     {{0.0,0.0,0.0,1.0},{ 0.25, 0.25, 0.25, 0.25}},
-    {{1.0,1.0,1.0,1.0},{ 1.00, 0.00, 0.00, 0.00}},
 };
 int numTestData = sizeof(testData)/sizeof(testData[0]);
 
@@ -28,6 +45,12 @@ bool crossOutputExpected(void){
     for(int i=0; i<numTestData; i++){
         float result[4];
         solveOutputs(testData[i][0], result);
+        /*
+        printf("input %f %f %f %f\n",testData[i][0][0],testData[i][0][1],testData[i][0][2],testData[i][0][3]);
+        printf("expct %f %f %f %f\n",testData[i][1][0],testData[i][1][1],testData[i][1][2],testData[i][1][3]);
+        printf("reslt %f %f %f %f\n",result[0],result[1],result[2],result[3]);
+        printf("\n");
+        */
         for(int j=0; j<4; j++){
             FPASSERT(testData[i][1][j], result[j]);
         }
